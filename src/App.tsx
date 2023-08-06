491bad3475501208ab1d3d381fdfe7c5833e53d1
Subsaw <subsaw@leprogrammers.com>
2023-08-06 21:56:35 +0200

    Removed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 function App() {
-  // const test = (x: number) => x;?
-  const message = 'Hello, world';
-  console.log(message);
   return (
     <div>
-      <p>HelloWorld</p>
+      <p>Hello World</p>
     </div>
   );
 }
