ecb7bdc3f3b7a982d258f49e2dc94747edf55576
Przemysław Światłoń <49292593+Swiatlon@users.noreply.github.com>
2023-08-06 21:20:28 +0200

    Initial frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  // const test = (x: number) => x;?
+  const message = 'Hello, world';
+  console.log(message);
+  return (
+    <div>
+      <p>HelloWorld</p>
+    </div>
+  );
+}
+
+export default App;
